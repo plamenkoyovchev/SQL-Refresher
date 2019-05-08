09c407224699649d0769dca49f57eae6e4829457
plamenyovchev <plamenkooo@gmail.com>
2019-05-08 23:06:34 +0300

    OuterJoinMultipleTables.sql

---
+++ OuterJoinMultipleTables.sql
@@ -0,0 +1,15 @@
+USE sql_store;
+
+SELECT 
+	o.order_date,
+    o.order_id,
+    c.first_name,
+    s.name AS shipper,
+    os.name as status
+FROM customers c
+JOIN orders o
+	ON c.customer_id = o.customer_id
+LEFT JOIN shippers s
+	ON o.shipper_id = s.shipper_id
+LEFT JOIN order_statuses os
+	ON o.status = os.order_status_id
