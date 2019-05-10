c3785e82bd26dfd65050a28c5b36ed60f7c13cc3
plamenyovchev <plamenkooo@gmail.com>
2019-05-10 08:18:30 +0300

    Add CopyTable.sql

---
+++ CopyTable.sql
@@ -0,0 +1,14 @@
+CREATE TABLE invoices_archived AS
+SELECT 
+	i.invoice_id,
+    i.number,
+    c.name AS client,
+    i.invoice_total,
+    i.payment_total,
+    i.invoice_date,
+    i.due_date,
+    i.payment_date
+FROM invoices i
+JOIN clients c 
+	USING(client_id)
+WHERE i.payment_date IS NOT NULL
