ae14650461ee080b1eb8df64642b3b935d08fa43
plamenyovchev <plamenkooo@gmail.com>
2019-05-11 17:50:29 +0300

    add view folder and clients_balance.sql view

---
+++ views/clients_balance.sql
@@ -0,0 +1,12 @@
+-- Create a view 'clients_balance' to see the balance for each client
+-- AS a rule always use group by when using aggregate functions(SUM,MAX,MIN etc.) 
+USE sql_invoicing;
+
+CREATE OR REPLACE VIEW clients_balance AS
+SELECT 
+	i.client_id,
+	c.name,
+    SUM(i.invoice_total - i.payment_total) AS balance
+FROM invoices i
+JOIN clients c USING(client_id)
+GROUP BY i.client_id, c.name;
