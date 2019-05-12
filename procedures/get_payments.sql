ce0a0ce2419297745e3fcdad7920c4325de4206e
plamenyovchev <plamenkooo@gmail.com>
2019-05-12 10:33:10 +0300

    add get payments

---
+++ procedures/get_payments.sql
@@ -0,0 +1,19 @@
+USE `sql_invoicing`;
+DROP procedure IF EXISTS `get_payments`;
+
+DELIMITER $$
+USE `sql_invoicing`$$
+CREATE PROCEDURE `get_payments` 
+(
+	client_id INT,
+    payment_method_id TINYINT
+)
+BEGIN
+	SELECT * 
+    FROM payments p
+    WHERE p.client_id = IFNULL(client_id, p.client_id)
+		  AND p.payment_method = IFNULL(payment_method_id, p.payment_method);
+END$$
+
+DELIMITER ;
+
